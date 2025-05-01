96847c1ee778772d657b4e821978c351d77985ae
agustina <agustina.leon.ext@farmatodo.com>
2025-05-01 15:30:58 -0300

    first commit

---
+++ src/components/AuthProvider.jsx
@@ -0,0 +1,60 @@
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useLayoutEffect,
+  useState,
+} from 'react';
+
+import api from '@/api';
+
+const AuthContext = createContext(undefined);
+
+export const useAuth = () => {
+  const authContext = useContext(AuthContext);
+
+  if (!authContext) {
+    throw new Error('useAuth must be used within a AuthProvider');
+  }
+
+  return authContext;
+};
+
+const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState();
+
+  useEffect(() => {
+    const fetchMe = async () => {
+      try {
+        const response = await api.get('/api/me');
+        setToken(response.data.accessToken);
+      } catch {
+        setToken(null);
+      }
+    };
+
+    fetchMe();
+  }, []);
+
+  useLayoutEffect(() => {
+    const authInterceptor = api.interceptors.request.use((config) => {
+      config.headers.Authorization =
+        !config._retry && token
+          ? `Bearer ${token}`
+          : config.headers.Authorization;
+      return config;
+    });
+
+    return () => {
+      api.interceptors.request.eject(authInterceptor);
+    };
+  }, [token]);
+
+  return (
+    <AuthContext.Provider value={{ token, setToken }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
---
+++ src/pages/ListingDetailsPage.jsx
@@ -0,0 +1,42 @@
+import { useParams } from 'react-router-dom';
+
+import DataRenderer from '@/components/DataRenderer';
+import ListingDetailsCard from '@/components/ListingDetailsCard';
+import { Spinner } from '@/components/ui';
+import useFetch from '@/hooks/useFetch';
+
+const ListingDetailsPage = () => {
+  const { listingId } = useParams();
+
+  const {
+    data: listing,
+    error,
+    isLoading,
+  } = useFetch(`/api/listings/${listingId}`);
+
+  const renderListing = () => {
+    if (isLoading) {
+      return (
+        <div className='flex justify-center'>
+          <Spinner size='sm' />
+        </div>
+      );
+    }
+
+    if (error) {
+      return <div className='text-center'>{error}</div>;
+    }
+
+    return <ListingDetailsCard listing={listing} />;
+  };
+
+  return (
+    <div className='container py-4'>
+      <DataRenderer error={error} isLoading={isLoading}>
+        <ListingDetailsCard listing={listing} />
+      </DataRenderer>
+    </div>
+  );
+};
+
+export default ListingDetailsPage;
